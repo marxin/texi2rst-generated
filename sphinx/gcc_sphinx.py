f583fbf5e6ecfc3af0431537910dadf417afd276
marxin <mliska@suse.cz>
2022-07-20 10:32:51 +0200

    Rename gcc-attr and gcc-param labels.

--- sphinx/gcc_sphinx.py
+++ sphinx/gcc_sphinx.py
@@ -3,9 +3,9 @@
 __version__ = '1.0'
 
 def setup(app):
-    app.add_object_type('gcc-attr', 'gcc-attr', objname='attribute',
+    app.add_object_type('gcc-attr', 'gcc-attr', objname='GCC attribute',
                         indextemplate='pair: %s; attribute')
-    app.add_object_type('gcc-param', 'gcc-param', objname='parameter',
+    app.add_object_type('gcc-param', 'gcc-param', objname='GCC parameter',
                         indextemplate='pair: %s; parameter')
 
     return dict(
